124dbcb86ad8b7eaecfb600be57eb4816b93954f
donggeunlim <rms0329@gmail.com>
2025-07-23 12:34:41 +0900

    init project page

---
+++ docs/js/bulma_toggle.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach((el) => {
+    el.addEventListener("click", () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle("is-active");
+      $target.classList.toggle("is-active");
+    });
+  });
+
+  // Get all elements with the class "toggle-button"
+  const $toggleButtons = Array.prototype.slice.call(document.querySelectorAll(".toggle-button"), 0);
+
+  // Add a click event on each of them
+  $toggleButtons.forEach((el) => {
+    el.addEventListener("click", () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on the button
+      el.classList.toggle("is-active");
+      // Toggle the "is-hidden" class on the target
+      $target.classList.toggle("is-hidden");
+    });
+  });
+});
